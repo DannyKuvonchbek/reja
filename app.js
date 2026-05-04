361878ccd6f6e43d6cae7bb19d4fa15d8f75d4b4
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-05-04 22:58:11 +0900

    :BBR reja loyixamizni yakunladik

--- app.js
+++ app.js
@@ -47,9 +47,24 @@ app.post("/delete-item", (req, res) => {
   );
 });
 
+app.post("/edit-item", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    db.collection("plans").findOneAndUpdate(
+    {_id: new mongodb.ObjectId(data.id) }, 
+    { $set: { reja: data.new_input } },
+    function (err, data) {
+        res.json({ state: "success" });
+    });
+});
+
+
 app.post("/delete-all", (req, res) => {
-    rejaList = [];
-    res.render("reja", { user: user, rejaList: rejaList });
+    if (req.body.delete_all) {
+        db.collection("plans").deleteMany(function () {
+            res.json({ state: "hamma rejalar o'chirildi"});
+      });
+    }
 });
 
 
